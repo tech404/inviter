544761e3b4dc177d25dfa159646edd0a6496e1de
Andy Lindeman <andy@lindeman.io>
2015-02-04 23:08:10 -0500

    Bootstraps our API

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,5 @@
+class InvitationsController < ApplicationController
+  def create
+    head :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :invitations, only: [:create]
+
   require "sidekiq/web"
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
