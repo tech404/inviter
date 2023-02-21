7a6675e1696522a00d850554910eb43a0c1a7707
Andy Lindeman <andy@lindeman.io>
2023-02-20 22:37:01 -0500

    Remove pg database, which was never used

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 ruby "2.7.5"
 
 gem 'rails', '5.0.7.2'
-gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,3 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
